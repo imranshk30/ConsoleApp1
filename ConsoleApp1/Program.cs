0705c1c9258308c97eb83807141c7c94d3e75fc8
imran sheikh <imranshk30@gmail.com>
2025-12-11 14:38:44 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//int i = 0;
+//while(i < 8)
+//{
+//    Console.WriteLine(i);
+//    i++;
+//}
+//do
+//{
+//    Console.WriteLine(i);
+//    i++;
+//} while (i < 8);
+//Console.ReadLine();
+//Console.WriteLine("Hello, World!");
+
+//for (; i < 8; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+//int i = 0;
+//do
+//{   
+//    Console.WriteLine(i);
+//i++;
+
+//}
+
+
+//while (i < 11);
+string[] family = { "imran sheikh","farah","hamzah" , "shanza" };
+
+foreach (var str in family)
+{
+    if (str == "imran sheikh")
+    {
+        Console.WriteLine("father");
+        
+    }
+    if(str == "farah")
+    {
+        Console.WriteLine("mother");
+    }
+    continue;
+    //else
+    //{
+
+
+    //    Console.WriteLine(number);
+    //}
+}
+Console.ReadLine();
